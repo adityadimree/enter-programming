f863b50de6d0f9c6f144fcddb9ffd26ec556e303
Aditya Dimree <adityadimree530@gmail.com>
2024-12-25 20:13:28 +0530

    Add files via upload

--- 03_project.py
+++ 03_project.py
@@ -1,42 +1,64 @@
+from collections import Counter
 import random
-import time
-
-print("Welcome to the hangman game. Here I will randomly select a word and then you have to guess what characters are present in the word. For every correct guess i will release the position of the character you guessed.")
-
-print("The word that i will select will belong to this list.")
-
 someWords = '''apple banana mango strawberry 
 orange grape pineapple apricot lemon coconut watermelon 
 cherry papaya berry peach lychee muskmelon'''
+someWords = someWords.split() 
+word = random.choice(someWords)
+chance = len(word)+2
+flag = 0
+guessed_char = []
+for i in word:
+  print("_", end=" ")
 
-my_list = someWords.split(" ")
-
-for i in my_list :
-    print(i)
-
-print("Now let me make a guess.")
-
-time.sleep(3)
-
-magic_word = random.choice(my_list)
-
-print("Guessed it !! Hint : It is a fruit's name.")
-
-tries = len(magic_word) + 2
-
-while tries >= 1 :
-    guess = input("Make a guess ! : ")
-    tries-=1
-    meset = set()
-    meset2 = set(magic_word)
-    if guess in magic_word :
-        print("You guessed it right !")
-        position = magic_word.index(guess) 
-        print(f"Position of the guess in the word is : {position}")
-        meset.add(guess)
-    if meset == meset2:
-        print(f"You have guessed all the letters !! You win.{magic_word} is the word.")
-        quit("See you in the next game.")
+#take in the input
+while chance>0:
+    guess = input("Please enter an alphabet : ")
+    chance-=1
+#guess evaluation gues is a single alphabet
+    if len(guess)!= 1:
+     print("Enter only a letter !!")
+     continue
+    if not guess.isalpha():
+     print("Enter only a alphabet !!")
+     continue
+    #adding the guess in the guessed list if it is correct and not if it is not correct
+    if guess in guessed_char:
+      print("Character already guessed !!")
+    elif guess in word:
+      guessed_char.append(guess)
+      flag+=1
     else:
-        print("Try again ! Tries remaining", tries)
-print("I winn!! All tries exhausted. The word was", magic_word)
+      print("Wrong guess go for another try !!")
+
+    for i in word:
+     if i in guessed_char:
+       print(i, end=" ")
+     else:
+      print("_", end=" ")
+    if flag == len(word):
+     print("You won!")
+     break
+print("Chances exhausted.")
+print(f"The word was :- {word}")
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+                
+
+            
+
+
+    
+
